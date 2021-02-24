a8c2dcd0f919d420b2f9d45b6867917547f524ae
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-02-24 22:33:59 +0530

    home-page-created

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chiranjiv's eyes</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo-menu">
+                <div class="logo">Chiranjiv's eyes</div>
+                <div class="menu">
+                    <a href="#">About us</a>
+                    <a href="#">More</a>
+                    <a href="#" id="admin">Admin</a>
+                </div>
+            </div>
+            <div class="main-header">
+                <p>The best photos shared by Chiranjiv Sharma.</p>
+                <div class="input">
+                    <input type="text" placeholder="Search Images...">
+                    <i class="fas fa-search"></i>
+                </div>
+            </div>
+            
+
+        </div>
+        <div class="type-section">
+            <div class="home">Home</div>
+            <div class="animal">Animals</div>
+            <div class="birds">Birds</div>
+            <div class="human">Human</div>
+        </div>
+        <div class="images">
+           <img src="./images/1.jpg">
+           <img src="./images/2.jpg">
+           <img src="./images/3.jpg">
+           <img src="./images/4.jpg">
+           <img src="./images/5.jpg">
+           <img src="./images/6.jpg">
+           <img src="./images/1.jpg">
+           <img src="./images/2.jpg">
+           <img src="./images/3.jpg">
+           <img src="./images/4.jpg">
+           <img src="./images/5.jpg">
+           <img src="./images/6.jpg">
+           <img src="./images/1.jpg">
+           <img src="./images/2.jpg">
+           <img src="./images/3.jpg">
+           <img src="./images/4.jpg">
+           <img src="./images/5.jpg">
+           <img src="./images/6.jpg">
+           <img src="./images/1.jpg">
+           <img src="./images/2.jpg">
+           <img src="./images/3.jpg">
+           <img src="./images/4.jpg">
+           <img src="./images/5.jpg">
+           <img src="./images/6.jpg">
+        </div>
+        <div class="footer">
+            <div class="social-media">
+                <i>I</i>
+                <i>F</i>
+                <i>T</i>
+            </div>
+            <h5>Chiranjivs's eyes</h5>
+            <div class="about-developer">
+                About Developer - <a href="#">Gopi Kumar Kaushik</a>
+            </div>
+        </div>
+    </div>
+
+ 
+
+</body>
+    <script src="./js/script.js"></script>
+</html>
---
+++ js/image.js
@@ -0,0 +1,10 @@
+export default function getImages(){
+    return [
+        {
+            id: "1.jpg",
+            name:"",
+            downloads:"",
+            
+        }
+    ]
+}
